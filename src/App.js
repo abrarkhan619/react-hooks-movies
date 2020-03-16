16c78ad465dbcdc35ef1a97a600d5fca37f5e319
abrakhan916 <abrar.khan916@gmail.com>
2020-03-16 16:51:20 +0000

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState(
+    [
+      {
+        name: "The Matrix",
+        year: 1999
+      },
+      {
+        name: "Harry Potter",
+        year: 2001
+      },
+      {
+        name: "Toy Story",
+        year: 1995
+      }
+    ]
+  );
+
+  const [filteredMovies, setFiltered] = useState([""])
+  
+  const changeMovie = () => {
+    const newState = [...movies]; // ... = spread operatory, it copies the array
+    
+    newState[1].name = "Gladiator";
+    newState[1].year = "1999";
+
+    setMovies(newState);
+  }
+
+  const searchMovies = (e) => { // common to use "e" short for event
+    // console.log(e.target.value);
+    
+    const getState = [...movies]
+     
+    // const moviesList = getState.toLowerCase()
+    // console.log(moviesList);
+    
+    const myFilter = getState.filter((movie) => {
+      let film = movie.name.toLowerCase();
+      let input = e.target.value.toLowerCase()
+      return film.indexOf(input) >= 0
+    });
+
+    // myfilter = ['cat', 'camel']
+    console.log(myFilter);
+    setFiltered(myFilter);
+    
+    // console.log(animal.indexOf(e.target.value));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Search for Movies</h1>
+      <input type="text" onChange={searchMovies}/>
+      <h2>Movies</h2>
+      {/* {movies.map( (movie) => {
+        return <h3>{movie.name} was released on {movie.year}</h3>
+      } )} */}
+      {/* <button onClick={changeMovie}>Change Movie</button> */}
+      {filteredMovies.map( (e) => {
+        return <h3>{e.name} was released on {e.year}</h3>
+      } )}
     </div>
   );
 }
